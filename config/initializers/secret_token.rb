2a6dba9adf5343e44db8f449ff6c1865aacdb66a
MichaelHsu <evenchange4@gmail.com>
2012-11-26 15:06:28 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Templet::Application.config.secret_token = '5953b73859ed442a79d4315707f2d01ffd49ca507e6f63a5babb2024b65599cc0938b111828c0db87bdef922821690bb9df383eb21616c6beb18ac2d5c5c85cb'
